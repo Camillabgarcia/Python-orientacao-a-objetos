3bf20265b6ab90fd018b2622f7614f44cd45dfa6
Camillabgarcia <camillabgarciaa@hotmail.com>
2024-02-21 17:27:00 -0300

    Métodos especiais

--- modelo/restaurante.py
+++ modelo/restaurante.py
@@ -1,10 +1,27 @@
-class  restaurante:              #CLASS: criando classe no python com atributos (nome, categoria, ativo) juntos.
-    nome = ''
-    categoria = ''
-    ativo = False
-restaurante_praca = restaurante()                 #Criando um novo restaurante que contenha as informações iguais a de restaurante().
-restaurante_pizza = restaurante()
+class Restaurante:  
+    # CLASS: Criando classe em Python com atributos (nome, categoria, ativo) juntos.
+    # SELF: Restaurante vai ter suas próprias informações.
+    # INIT: É usado para inicializar os atributos do objeto.
+    def __init__(self, nome, categoria):
+        self.nome = nome  # Atributos (características)
+        self.categoria = categoria  # Atributos
+        self.ativo = False  # Atributos
+
+    # __STR__: Pega o objeto e define que, se precisarmos mostrar esse objeto em formato de texto,
+    # mostraremos determinada informação.
+    def __str__(self):
+        return f'{self.nome} | {self.categoria}'
+
+restaurante_praca = Restaurante('Praça', 'Gourmet')
+# Criando um novo restaurante que contenha os atributos iguais ao de Restaurante().
+restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
+# Objeto (restaurante_pizza) criado com a classe (Restaurante).
 
 restaurantes = [restaurante_praca, restaurante_pizza]
 
-print(restaurantes)
+# print(vars(restaurante_praca))  # VARS: Ver um dicionário (atributo) desse objeto.
+
+print(restaurante_praca)
+print(restaurante_pizza)
+
+
