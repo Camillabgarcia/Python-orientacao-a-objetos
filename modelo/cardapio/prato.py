20c0eed5b2bd7485956c7638fb52aae1d8db1ad1
Camillabgarcia <camillabgarciaa@hotmail.com>
2024-02-26 16:21:26 -0300

    Herança

--- modelo/cardapio/prato.py
+++ modelo/cardapio/prato.py
@@ -1,3 +1,6 @@
-class Prato:
+from modelo.cardapio.item_cardapio import ItemCardapio                   
+                                     
+class Prato(ItemCardapio):                           #A classe Prato vai herdar informações e atributos da classe item_cardapio.
     def __init__(self, nome, preco, descricao):
-        pass
+        super().__init__(nome, preco)               # Super: permite acessar informações de outra classe.
+        self.descricao = descricao
